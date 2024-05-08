38af7943b2129cc254d29ac9a90fe0bd09be3b07
柒哥 <119522504+TikTok365@users.noreply.github.com>
2024-05-08 22:02:50 +0800

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -101,7 +101,7 @@ function Goodbye() {
     echo -e "${Font_Green}✅ ${Font_Suffix} ${Font_SkyBlue}各国千分满月账户\自然流\万粉号${Font_Suffix}"
     echo -e "${Font_Green}✅ ${Font_Suffix} ${Font_SkyBlue}各国MCN\小店代入驻${Font_Suffix}"
     echo ""
-    echo -e "${Font_Yellow}联系咨询: https://t.me/zhuyuheng${Font_Suffix}"
+    echo -e "${Font_Yellow}联系咨询: https://t.me/kuajingqige${Font_Suffix}"
     echo -e "================================================"
     echo -e ""
 }
